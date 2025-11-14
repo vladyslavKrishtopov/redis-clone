571b2dca177ed7ba6bebbb91a7b63ecfbe2458e6
vladyslavKrishtopov <krishtopov.vlad@gmail.com>
2025-11-14 10:10:56 -0600

    added basic server/client functionality

---
+++ client.cpp
@@ -0,0 +1,42 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+
+static void die(const char* msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+
+int main(){
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        die("socket()");
+    }
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
+    }
+
+    char msg[] = "hello";
+    write(fd, msg, strlen(msg));
+
+    char rbuf[64] = {};
+    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        die("read");
+    }
+    printf("server says: %s\n", rbuf);
+    close(fd);
+
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,70 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+
+static void die(const char* msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
+}
+
+// Read & Write
+static void do_something(int connfd) {
+    char rbuf[64] = {};
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0) {
+        msg("read() error");
+        return;
+    }
+    printf("client says: %s\n", rbuf);
+
+    char wbuf[] = "world";
+    write(connfd, wbuf, strlen(wbuf));
+}
+
+
+int main() {
+    // Obtaining a socket handle
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Setting socket options
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
+    // Bind to an adress
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);        // port
+    addr.sin_addr.s_addr = htonl(0);    // wildcard IP 0.0.0.0
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) { die("bind()"); }
+
+    // Listen for incoming connections
+    rv = listen(fd, SOMAXCONN);
+    if (rv) { die("listen()"); }
+
+    // Accept connections
+    while (true) {
+        struct sockaddr_in client_addr = {};
+        socklen_t addrlen = sizeof(client_addr);
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0){
+            continue; // error
+        }
+
+        do_something(connfd);
+        close(connfd);
+    }
+
+    return 0;
+}
